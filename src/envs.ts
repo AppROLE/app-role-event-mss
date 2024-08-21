92dfda2d4df539d83a765ddcab27c50702557b77
LucaPinheiro <lucapgomes11@gmail.com>
2024-08-20 21:05:02 -0600

    chore: Update envs.ts to include GITHUB_REF environment variable

--- src/envs.ts
+++ src/envs.ts
@@ -7,6 +7,7 @@ config({ path: path.resolve(__dirname, "../.env") });
 
 export const envs = {
   AWS_ACCOUNT_ID: process.env.AWS_ACCOUNT_ID,
+  GITHUB_REF: process.env.GITHUB_REF_NAME,
   STACK_NAME: process.env.STACK_NAME,
   AWS_REGION: process.env.AWS_REGION,
   STAGE: process.env.STAGE,
