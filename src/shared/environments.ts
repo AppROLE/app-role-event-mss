d4c143d5f450e04d7a18c9b32873e5b75bd52d12
LucaPinheiro <lucapgomes11@gmail.com>
2024-09-05 00:16:16 -0300

    feat: Add Environments class for managing environment variables and configurations

---
+++ src/shared/environments.ts
@@ -0,0 +1,68 @@
+import { STAGE } from "./domain/enums/stage_enum";
+import { IEventRepository } from "./domain/irepositories/event_repository_interface";
+import { envs } from "./helpers/envs/envs";
+import { EventRepositoryMongo } from "./infra/database/repositories/event_repository_mongo";
+
+export class Environments {
+  stage: STAGE = STAGE.TEST;
+  s3BucketName: string = "";
+  region: string = "";
+  EventPoolId: string = "";
+  clientId: string = "";
+  mongoUri: string = "";
+  cloudFrontUrl: string = "";
+
+  configureLocal() {
+    console.log(
+      "envs.STAGE - [ENVIRONMENTS - { CONFIGURE LOCAL }] - ",
+      envs.STAGE
+    );
+    envs.STAGE = envs.STAGE || "TEST";
+  }
+
+  loadEnvs() {
+    if (!envs.STAGE) {
+      this.configureLocal();
+    }
+
+    this.stage = envs.STAGE as STAGE;
+
+    if (this.stage === STAGE.TEST) {
+      this.s3BucketName = "bucket-test";
+      this.region = "sa-east-1";
+    } else {
+      this.s3BucketName = (envs.S3_BUCKET_NAME.toLowerCase() +
+        envs.STAGE.toLowerCase()) as string;
+      this.region = envs.AWS_REGION as string;
+      this.clientId = envs.COGNITO_CLIENT_ID as string;
+      this.mongoUri = envs.MONGO_URI as string;
+      this.cloudFrontUrl = `https://${envs.CLOUDFRONT_DISTRO}`;
+    }
+  }
+
+  static getEventRepo(): IEventRepository {
+    console.log(
+      "Environments.getEnvs().stage - [ENVIRONMENTS - { GET Event REPO }] - ",
+      Environments.getEnvs().stage
+    );
+
+    if (Environments.getEnvs().stage === STAGE.TEST) {
+      throw new Error("Invalid STAGE");
+    } else if (
+      Environments.getEnvs().stage === STAGE.DEV ||
+      Environments.getEnvs().stage === STAGE.PROD
+    ) {
+      return new EventRepositoryMongo();
+    } else {
+      throw new Error("Invalid STAGE");
+    }
+  }
+
+  static getEnvs() {
+    const envs = new Environments();
+    envs.loadEnvs();
+
+    console.log("envs - [ENVIRONMENTS - { GET ENVS }] - ", envs);
+    return envs;
+  }
+}
