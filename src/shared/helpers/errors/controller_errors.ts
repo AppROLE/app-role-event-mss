2f235f6462e9b64a0fd4af642785cd9b00f91bbf
LucaPinheiro <lucapgomes11@gmail.com>
2024-09-05 00:16:35 -0300

    refactor: Update error messages in controller_errors.ts

--- src/shared/helpers/errors/controller_errors.ts
+++ src/shared/helpers/errors/controller_errors.ts
@@ -1,24 +1,8 @@
 import { BaseError } from "./base_error";
 
-export class InvalidRequest extends BaseError {
-  constructor(parameter?: string) {
-      if (parameter) {
-          super(parameter + " not found");
-      } else {
-          super("No request found");
-      }
-  }
-}
-
-export class InvalidParameter extends BaseError {
-  constructor(parameter: string, value: string) {
-      super(`Invalid parameter: ${parameter}: ${value}`);
-  }
-}
-
 export class MissingParameters extends BaseError {
   constructor(message: string) {
-    super(`Field ${message} is missing`);
+    super(`O campo ${message} está faltando.`);
   }
 }
 
@@ -29,7 +13,7 @@ export class WrongTypeParameters extends BaseError {
     fieldTypeReceived: string
   ) {
     super(
-      `Field ${fieldName} isn't in the right type.\n Received: ${fieldTypeReceived}.\n Expected: ${fieldTypeExpected}.`
+      `O campo ${fieldName} não possui o tipo correto.\n Recebido: ${fieldTypeReceived}.\n Experado: ${fieldTypeExpected}.`
     );
   }
 }
