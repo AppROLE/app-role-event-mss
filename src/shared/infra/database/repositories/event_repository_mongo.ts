cd4be9df7176b21ee2cb70420334ead13411af17
LucaPinheiro <lucapgomes11@gmail.com>
2024-09-05 00:33:21 -0300

    refactor: Update EventRepositoryMongo to implement IEventRepository interface

--- src/shared/infra/database/repositories/event_repository_mongo.ts
+++ src/shared/infra/database/repositories/event_repository_mongo.ts
@@ -1,15 +1,12 @@
-// src/repositories/eventRepositoryMongo.ts
-import { IEvent } from "../models/event.model"; // Importando o modelo de Evento
-import { Event } from "../../../domain/entities/event"; // Importe da classe Event do domínio, se existir
-import { EventMongoDTO } from "../dtos/event_mongo_dto"; // Importe do DTO adequado, se existir
-import {
-  DuplicatedItem,
-  NoItemsFound,
-} from "../../../helpers/errors/usecase_errors"; // Importe os erros necessários
-import { v4 as uuidv4 } from "uuid"; // Importe a função uuidv4
-import { connectDB } from "../models";
+import { IEvent } from "../models/event.model";
+import { Event } from "../../../domain/entities/event";
+import { EventMongoDTO } from "../dtos/event_mongo_dto";
 
-export class EventRepositoryMongo {
+import { v4 as uuidv4 } from "uuid";
+import { connectDB } from "../models";
+import { IEventRepository } from "../../../domain/irepositories/event_repository_interface";
+
+export class EventRepositoryMongo implements IEventRepository {
   async createEvent(event: Event): Promise<Event> {
     try {
       const db = await connectDB();
@@ -22,20 +19,16 @@ export class EventRepositoryMongo {
         db.connections[0].db?.collection<IEvent>("Event");
 
       const dto = EventMongoDTO.fromEntity(event);
-      const eventDoc = EventMongoDTO.toMongo(dto); // Converter o DTO para o formato adequado do MongoDB
-
-      console.log("MONGO REPO EVENT DOC: ", eventDoc);
+      const eventDoc = EventMongoDTO.toMongo(dto);
 
       eventDoc._id = uuidv4();
 
       const respMongo = await eventMongoClient?.insertOne(eventDoc);
       console.log("MONGO REPO EVENT RESPMONGO: ", respMongo);
-      console.log("MONGO REPO EVENT CREATED: ", event);
 
       return event;
     } catch (error) {
       throw new Error(`Error creating event on MongoDB: ${error}`);
-    } finally {
     }
   }
 }
