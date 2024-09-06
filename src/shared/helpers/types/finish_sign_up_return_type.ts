769f45a0dcc827be85f36aa285b7f85db26c186f
LucaPinheiro <lucapgomes11@gmail.com>
2024-09-05 22:57:00 -0300

    refactor: Remove finish_sign_up_return_type.ts file and related code

--- src/shared/helpers/types/finish_sign_up_return_type.ts
+++
@@ -1,8 +0,0 @@
-export type FinishSignUpReturnType = {
-  email: string;
-  newUsername: string;
-  newNickname: string;
-  acceptedTerms: string | undefined;
-  roleType: string | undefined;
-  name: string;
-}
