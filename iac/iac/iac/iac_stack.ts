5cb064d4474a9ee0367ae33068ae73bd98f9409d
LucaPinheiro <lucapgomes11@gmail.com>
2024-09-05 22:55:17 -0300

    Refactor IacStack to remove CloudFront distribution and related code

--- iac/iac/iac/iac_stack.ts
+++ iac/iac/iac/iac_stack.ts
@@ -7,7 +7,6 @@ import { LambdaStack } from "./lambda_stack";
 import { Stack, StackProps } from "aws-cdk-lib";
 import { envs } from "../../../src/shared/helpers/envs/envs";
 import { Cors, RestApi } from "aws-cdk-lib/aws-apigateway";
-import { Distribution, OriginAccessIdentity } from "aws-cdk-lib/aws-cloudfront";
 
 export class IacStack extends Stack {
   constructor(scope: Construct, constructId: string, props?: StackProps) {
@@ -30,53 +29,6 @@ export class IacStack extends Stack {
         allowHeaders: Cors.DEFAULT_HEADERS,
       },
     });
-    const s3Stack = new S3Stack(this, `${envs.STACK_NAME}-S3Stack`);
-
-    const s3Policy = new iam.PolicyStatement({
-      effect: iam.Effect.ALLOW,
-      actions: ["s3:*"],
-      resources: [s3Stack.bucket.bucketArn],
-    });
-
-    const originAccessIdentity = new OriginAccessIdentity(
-      this,
-      "AppRoleFrontOAI",
-      {
-        comment: "OAI for S3 bucket",
-      }
-    );
-
-    const distribution = new Distribution(this, "AppRoleFrontDistribution", {
-      defaultBehavior: {
-        origin: new cdk.aws_cloudfront_origins.S3Origin(s3Stack.bucket, {
-          originAccessIdentity: originAccessIdentity,
-        }),
-        allowedMethods: cdk.aws_cloudfront.AllowedMethods.ALLOW_GET_HEAD,
-        cachedMethods: cdk.aws_cloudfront.CachedMethods.CACHE_GET_HEAD,
-        viewerProtocolPolicy:
-          cdk.aws_cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-        cachePolicy: cdk.aws_cloudfront.CachePolicy.CACHING_OPTIMIZED,
-      },
-    });
-
-    distribution.applyRemovalPolicy(cdk.RemovalPolicy.DESTROY);
-
-    s3Stack.bucket.addToResourcePolicy(
-      new iam.PolicyStatement({
-        effect: iam.Effect.ALLOW,
-        actions: ["s3:GetObject"],
-        resources: [s3Stack.bucket.arnForObjects("*")],
-        principals: [
-          new iam.ServicePrincipal("cloudfront.amazonaws.com", {
-            conditions: {
-              StringEquals: {
-                "AWS:SourceArn": `arn:aws:cloudfront::${this.account}:distribution/${distribution.distributionId}`,
-              },
-            },
-          }),
-        ],
-      })
-    );
 
     const environmentVariables = {
       STAGE: stage,
@@ -84,8 +36,6 @@ export class IacStack extends Stack {
       EMAIL_LOGIN: envs.EMAIL_LOGIN,
       EMAIL_PASSWORD: envs.EMAIL_PASSWORD,
       MONGO_URI: envs.MONGO_URI,
-      S3_BUCKET_NAME: envs.S3_BUCKET_NAME + "-" + stage.toLowerCase(),
-      CLOUDFRONT_DISTRO: distribution.domainName,
     };
 
     const lambdaStack = new LambdaStack(
@@ -93,15 +43,5 @@ export class IacStack extends Stack {
       apigatewayResource,
       environmentVariables
     );
-    
-    for (const fn of lambdaStack.functionsThatNeedS3Permissions) {
-      fn.addToRolePolicy(s3Policy);
-    }
-
-    new cdk.CfnOutput(this, "AppRoleFrontDistributionOutput", {
-      value: distribution.domainName,
-      description: "The distribution domain",
-      exportName: "AppRoleFrontDistributionOutput",
-    });
   }
 }
