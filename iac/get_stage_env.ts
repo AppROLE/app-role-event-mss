a4478d1e27e6f92e36a523a744fef1ce8a98b008
LucaPinheiro <lucapgomes11@gmail.com>
2024-09-09 18:06:50 -0300

    Add get_stage_env.ts to determine the deployment stage based on the GITHUB_REF environment variable

---
+++ iac/get_stage_env.ts
@@ -0,0 +1,15 @@
+import { envs } from "../src/shared/helpers/envs/envs";
+
+let stage: string;
+
+if (envs.GITHUB_REF.includes("prod")) {
+  stage = "PROD";
+} else if (envs.GITHUB_REF.includes("homolog")) {
+  stage = "HOMOLOG";
+} else if (envs.GITHUB_REF.includes("dev")) {
+  stage = "DEV";
+} else {
+  stage = "TEST";
+}
+
+export { stage };
