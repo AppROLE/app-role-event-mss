536e588fc5f6724aed3b074838d39fa2fe9d5485
gabrielmerola <gabrielmerola12@gmail.com>
2024-09-09 23:15:05 -0300

    feat(institute): update institute type enum
    Update the institute type enum in the domain layer to include new types or modify existing ones. This change enhances the flexibility and accuracy of institute categorization within the system.

--- src/shared/domain/enums/institute_type_enum.ts
+++ src/shared/domain/enums/institute_type_enum.ts
@@ -1,14 +1,14 @@
 export enum INSTITUTE_TYPE {
-    TYPE_1 = "type_1",
-    TYPE_2 = "type_2"
+    ESTABELECIMENTO_FIXO = "ESTABELECIMENTO_FIXO",
+    AGENCIA_DE_FESTAS = "AGENCIA_DE_FESTAS"
 }
 
 export function toEnum(value: string): INSTITUTE_TYPE {
     switch(value) {
-        case "type_1":
-            return INSTITUTE_TYPE.TYPE_1;
-        case "type_2":
-            return INSTITUTE_TYPE.TYPE_2;
+        case "ESTABELECIMENTO_FIXO":
+            return INSTITUTE_TYPE.ESTABELECIMENTO_FIXO;
+        case "AGENCIA_DE_FESTAS":
+            return INSTITUTE_TYPE.AGENCIA_DE_FESTAS;
         default:
             throw new Error("Invalid institute type");
     }
