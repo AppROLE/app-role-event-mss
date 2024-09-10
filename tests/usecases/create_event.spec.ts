dbce504856120c3e167f62e7c8f1316f41450e60
LucaPinheiro <lucapgomes11@gmail.com>
2024-09-09 21:55:42 -0300

    feat: Add CreateEventUseCase test

---
+++ tests/usecases/create_event.spec.ts
@@ -0,0 +1,49 @@
+import { describe, it, expect } from "vitest";
+import { Event } from "../../src/shared/domain/entities/event";
+import { STATUS } from "../../src/shared/domain/enums/status_enum";
+import { IEventRepository } from "../../src/shared/domain/irepositories/event_repository_interface";
+import { CreateEventUseCase } from "../../src/modules/create_event/app/create_event_usecase";
+import { EventRepositoryMock } from "../../src/shared/infra/mocks/event_repository_mock";
+
+describe("CreateEventUseCase", () => {
+  it("should create and save an event", async () => {
+    const mockRepo: IEventRepository = new EventRepositoryMock();
+    const usecase = new CreateEventUseCase(mockRepo);
+
+    const params = {
+      name: "Galleria Night",
+      description:
+        "Galleria club. A melhor balada de São Paulo. Venha curtir com a gente!",
+      address: "987 Club St, Downtown",
+      price: 80.0,
+      ageRange: "21+",
+      eventDate: new Date("2025-09-15"),
+      districtId: "1",
+      instituteId: "1",
+      eventStatus: STATUS.ACTIVE,
+      bannerUrl: "https://example.com/galleria_night.jpg",
+    };
+
+    const result = await usecase.execute(params);
+    console.log("CARALHO" + typeof result);
+    console.log("Result type:", result.constructor.name);
+
+    expect(result).toBeInstanceOf(Event);
+    expect(result.name).toBe("Galleria Night");
+    expect(result.description).toBe(
+      "Galleria club. A melhor balada de São Paulo. Venha curtir com a gente!"
+    );
+    expect(result.address).toBe("987 Club St, Downtown");
+    expect(result.price).toBe(80.0);
+    expect(result.ageRange).toBe("21+");
+    expect(result.eventDate).toEqual(new Date("2025-09-15"));
+    expect(result.districtId).toBe("1");
+    expect(result.instituteId).toBe("1");
+    expect(result.eventStatus).toBe(STATUS.ACTIVE);
+    expect(result.bannerUrl).toBe("https://example.com/galleria_night.jpg");
+
+    const events = await mockRepo.getAllEvents();
+    expect(events).toHaveLength(1);
+    expect(events[0]).toEqual(result);
+  });
+});
