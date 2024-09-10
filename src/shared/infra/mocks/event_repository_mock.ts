2cab78eef03eaf09916f487cd4cce6fe7ea34294
LucaPinheiro <lucapgomes11@gmail.com>
2024-09-09 21:55:34 -0300

    feat: Add getAllEvents method to EventRepositoryMock class

--- src/shared/infra/mocks/event_repository_mock.ts
+++ src/shared/infra/mocks/event_repository_mock.ts
@@ -1,11 +1,21 @@
 import { Event } from "../../domain/entities/event";
 import { IEventRepository } from "../../domain/irepositories/event_repository_interface";
+import { EventMock } from "../../domain/mocks/event_mock";
 
 export class EventRepositoryMock implements IEventRepository {
-  private events: Event[] = [];
+  private events: Event[];
+
+  constructor() {
+    const eventMock = new EventMock();
+    this.events = eventMock.events;
+  }
 
   createEvent(event: Event): Promise<Event> {
     this.events.push(event);
     return Promise.resolve(event);
+  }
+
+  async getAllEvents(): Promise<Event[]> {
+    return [...this.events];
   }
 }
