eba88e4ffe48a638f0565d4f005ac12a8661a596
LucaPinheiro <lucapgomes11@gmail.com>
2024-09-10 20:45:37 -0300

    Refactor LambdaStack to add getAllEventsFunction

--- iac/stacks/lambda_stack.ts
+++ iac/stacks/lambda_stack.ts
@@ -14,6 +14,7 @@ export class LambdaStack extends Construct {
 
   healthCheckFunction: lambda.Function
   createEventFunction: lambda.Function
+  getAllEventsFunction: lambda.Function
 
   createLambdaApiGatewayIntegration(
     moduleName: string, 
@@ -65,6 +66,7 @@ export class LambdaStack extends Construct {
     // auth routes
     this.healthCheckFunction = this.createLambdaApiGatewayIntegration('health_check', 'GET', apiGatewayResource, environmentVariables)
     this.createEventFunction = this.createLambdaApiGatewayIntegration('create_event', 'POST', apiGatewayResource, environmentVariables)
+    this.getAllEventsFunction = this.createLambdaApiGatewayIntegration('get_all_events', 'GET', apiGatewayResource, environmentVariables)
 
     this.functionsThatNeedS3Permissions = []
 
