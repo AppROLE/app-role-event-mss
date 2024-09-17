76658ab03ecd67e9ea368486b4a2aec8d02abd1a
gabrielmerola <gabrielmerola12@gmail.com>
2024-09-17 16:04:55 -0300

    add create institute presenter

---
+++ src/modules/create_institute/app/create_institute_presenter.ts
@@ -0,0 +1,29 @@
+import { Environments } from "src/shared/environments";
+import {
+  LambdaHttpRequest,
+  LambdaHttpResponse,
+} from "src/shared/helpers/external_interfaces/http_lambda_requests";
+import { CreateInstituteUseCase } from "./create_institute_usecase";
+import { CreateInstituteController } from "./create_institute_controller";
+
+const repo = Environments.getInstituteRepo();
+const usecase = new CreateInstituteUseCase(repo);
+const controller = new CreateInstituteController(usecase);
+
+export async function createEventPresenter(event: Record<string, any>) {
+    const httpRequest = new LambdaHttpRequest(event);
+
+    const response = await controller.handle(httpRequest)
+    const httpResponse = new LambdaHttpResponse(
+        response?.body,
+        response?.statusCode,
+        response?.headers
+    )
+
+    return httpResponse.toJSON()
+}
+
+export async function lambda_handler(event: any, context: any) {
+    const response = await createEventPresenter(event);
+    return response;
+}
