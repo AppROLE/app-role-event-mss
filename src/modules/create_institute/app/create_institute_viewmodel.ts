3b28a94428370518278e8c72b5c392895e21f2b5
gabrielmerola <gabrielmerola12@gmail.com>
2024-09-17 16:04:41 -0300

    add create institute viewmodel

---
+++ src/modules/create_institute/app/create_institute_viewmodel.ts
@@ -0,0 +1,13 @@
+export class CreateInstituteViewModel {
+    message: string;
+
+    constructor(message: string) {
+        this.message = message;
+    }
+
+    toJSON() {
+        return {
+            message: this.message,
+        };
+    }
+}
