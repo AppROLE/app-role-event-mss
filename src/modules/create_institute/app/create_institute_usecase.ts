4a9f93bfc64343ec99f512ca121a5f4ba79ebd83
gabrielmerola <gabrielmerola12@gmail.com>
2024-09-17 16:04:19 -0300

    add create institute usecase

---
+++ src/modules/create_institute/app/create_institute_usecase.ts
@@ -0,0 +1,34 @@
+import { Institute } from "src/shared/domain/entities/institute";
+import { IInstituteRepository } from "src/shared/domain/irepositories/institute_repository_interface";
+
+export interface CreateInstituteParams {
+    name: string;
+    logo_photo: string;
+    description: string;
+    institute_type: string;
+    address?: string;
+    price?: number;
+    district_id?: string;
+    photos_url?: string[];
+}
+
+export class CreateInstituteUseCase {
+    constructor(private repo: IInstituteRepository) {}
+    
+    async execute(params: CreateInstituteParams): Promise<Institute> {
+        const institute = new Institute({
+            name: params.name,
+            description: params.description,
+            institute_type: params.institute_type,
+            address: params.address,
+            logo_photo: params.logo_photo,
+            district_id: params.district_id,
+            photos_url: params.photos_url,
+            price: params.price,
+        });
+    
+        const savedInstitute = await this.repo.createInstitute(institute);
+        
+        return savedInstitute;
+    }
+}
