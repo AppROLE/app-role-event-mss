e2d5524d6465fb6ba84793a31dad9465fa44b8d8
LucaPinheiro <lucapgomes11@gmail.com>
2024-09-16 23:53:29 -0300

    Refactor IEventRepository to include getEventById method

--- src/shared/domain/irepositories/event_repository_interface.ts
+++ src/shared/domain/irepositories/event_repository_interface.ts
@@ -3,4 +3,5 @@ import { Event } from "../entities/event";
 export interface IEventRepository {
   createEvent(event: Event): Promise<Event>;
   getAllEvents(): Promise<Event[]>;
+  getEventById(eventId: string): Promise<Event>;
 }
