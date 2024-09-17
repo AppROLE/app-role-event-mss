3f867b46edf593884ccc10d60b84be91a66c0f4b
LucaPinheiro <lucapgomes11@gmail.com>
2024-09-17 17:27:58 -0300

    Add DeleteEventByIdPresenter to handle deleting events by ID

---
+++ src/modules/delete_event_by_id/app/delete_event_by_id_presenter.ts
@@ -0,0 +1,28 @@
+import { Environments } from "src/shared/environments";
+import {
+  LambdaHttpRequest,
+  LambdaHttpResponse,
+} from "src/shared/helpers/external_interfaces/http_lambda_requests";
+import { DeleteEventByIdUseCase } from "./delete_event_by_id_usecase";
+import { DeleteEventByIdController } from "./delete_event_by_id_controller";
+
+const repo = Environments.getEventRepo();
+const usecase = new DeleteEventByIdUseCase(repo);
+const controller = new DeleteEventByIdController(usecase);
+
+export async function deleteEventByIdPresenter(event: Record<string, any>) {
+  const httpRequest = new LambdaHttpRequest(event);
+  const response = await controller.handle(httpRequest);
+  const httpResponse = new LambdaHttpResponse(
+    response?.body,
+    response?.statusCode,
+    response?.headers
+  );
+
+  return httpResponse.toJSON();
+}
+
+export async function lambda_handler(event: any, context: any) {
+  const response = await deleteEventByIdPresenter(event);
+  return response;
+}
