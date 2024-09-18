cdf303e1abb599d6ee3ac56c88def55f40b6c3e0
LucaPinheiro <lucapgomes11@gmail.com>
2024-09-18 17:56:42 -0300

    Refactor getPhrase method in PhraseRepositoryMongo

--- src/shared/infra/database/repositories/phrase_repository_mongo.ts
+++ src/shared/infra/database/repositories/phrase_repository_mongo.ts
@@ -1,26 +1,30 @@
 import { connectDB } from "../models";
 import { IPhrase } from "../models/phrase.model";
 import { IPhraseRepository } from "../../../domain/irepositories/phrase_repository_interface";
+import { NoItemsFound } from "src/shared/helpers/errors/usecase_errors";
 
 export class PhraseRepositoryMongo implements IPhraseRepository {
-    async getPhrase(): Promise<IPhrase | null> {
-      try {
-        const db = await connectDB();
-        db.connections[0].on("error", () => {
-          console.error.bind(console, "connection error:");
-          throw new Error("Error connecting to MongoDB");
-        });
-  
-        const phraseMongoClient = db.connections[0].db?.collection<IPhrase>("Phrase");
-  
-        const randomPhrase = await phraseMongoClient
-          ?.aggregate([{ $sample: { size: 1 } }])
-          .toArray();
-  
-        return randomPhrase ? randomPhrase[0] as IPhrase : null;
-      } catch (error) {
-        throw new Error(`Error retrieving random phrase from MongoDB: ${error}`);
+  async getPhrase(): Promise<IPhrase | null> {
+    try {
+      const db = await connectDB();
+      db.connections[0].on("error", () => {
+        console.error.bind(console, "connection error:");
+        throw new Error("Error connecting to MongoDB");
+      });
+
+      const phraseMongoClient = db.connections[0].db?.collection<IPhrase>("Phrase");
+
+      const randomPhrase = await phraseMongoClient
+        ?.aggregate([{ $sample: { size: 1 } }])
+        .toArray();
+
+      if (!randomPhrase || randomPhrase.length === 0) {
+        throw new NoItemsFound("phrase in db");
       }
+
+      return randomPhrase[0] as IPhrase;
+    } catch (error) {
+      throw new Error(`Error retrieving random phrase from MongoDB: ${error}`);
     }
   }
-  
+}
