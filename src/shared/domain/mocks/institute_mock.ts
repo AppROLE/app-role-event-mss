4a481439e0fbe220cfab505fcff96811cf3d12f4
gabrielmerola <gabrielmerola12@gmail.com>
2024-09-20 16:47:37 -0300

    resolve mock

--- src/shared/domain/mocks/institute_mock.ts
+++ src/shared/domain/mocks/institute_mock.ts
@@ -1,6 +1,7 @@
 
 import { Institute } from "../entities/institute";
 import { INSTITUTE_TYPE } from "../enums/institute_type_enum";
+import { PARTNER_TYPE } from "../enums/partner_type_enum";
 
 export class InstituteMock {
   public institutes: Institute[];
@@ -9,18 +10,21 @@ export class InstituteMock {
     this.institutes = [
       new Institute({
         name: "Galleria Club",
+        partner_type: PARTNER_TYPE.NO_PARTNER,
         description: "A melhor balada de São Paulo",
         institute_type: INSTITUTE_TYPE.ESTABELECIMENTO_FIXO,
         logo_photo: "https://example.com/galleria_club_logo.jpg",
       }),
       new Institute({
         name: "Galleria Club",
+        partner_type: PARTNER_TYPE.PROMOTER_PARTNER,
         description: "A melhor balada de São Paulo",
         institute_type: INSTITUTE_TYPE.AGENCIA_DE_FESTAS,
         logo_photo: "https://example.com/galleria_club_logo.jpg",
       }),
       new Institute({
         name: "Galleria Club",
+        partner_type: PARTNER_TYPE.GLOBAL_PARTNER,
         description: "A melhor balada de São Paulo",
         institute_type: INSTITUTE_TYPE.ESTABELECIMENTO_FIXO,
         logo_photo: "https://example.com/galleria_club_logo.jpg",
