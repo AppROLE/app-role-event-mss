e83cc550989791092e298a0d60758e4f22910a71
gabrielmerola <gabrielmerola12@gmail.com>
2024-10-02 00:40:35 -0300

    add deleteInstituteById to interface

--- src/shared/domain/irepositories/institute_repository_interface.ts
+++ src/shared/domain/irepositories/institute_repository_interface.ts
@@ -3,4 +3,5 @@ import { Institute } from "../entities/institute";
 export interface IInstituteRepository {
   createInstitute(institute: Institute): Promise<Institute>;
   getInstituteById(instituteId: string): Promise<Institute>;
+  deleteInstituteById(instituteId: string): Promise<void>;
 }
