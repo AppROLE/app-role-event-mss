cb4e4dfce6bc3f4b8f9f01d66bbbfb934f4f269b
gabrielmerola <gabrielmerola12@gmail.com>
2024-10-02 01:10:28 -0300

    add getAllInstitutes viewmodel

---
+++ src/modules/get_all_institutes/app/get_all_institutes_viewmodel.ts
@@ -0,0 +1,64 @@
+import { INSTITUTE_TYPE } from "src/shared/domain/enums/institute_type_enum";
+import { Event } from "../../../shared/domain/entities/event";
+import { PARTNER_TYPE } from "src/shared/domain/enums/partner_type_enum";
+import { Institute } from "src/shared/domain/entities/institute";
+
+export class InstituteViewModel {
+    private institute_id?: string;
+    private name: string;
+    private logo_photo?: string;
+    private description: string;
+    private institute_type: INSTITUTE_TYPE;
+    private partner_type: PARTNER_TYPE;
+    private address?: string;
+    private price?: number;
+    private district_id?: string;
+    private photos_url?: string[];
+    private events_id?: string[];
+  
+    constructor(institute: Institute) {
+      this.institute_id = institute.instituteId;
+      this.name = institute.instituteName;
+      this.logo_photo = institute.instituteLogoPhoto;
+      this.description = institute.instituteDescription;
+      this.institute_type = institute.instituteInstituteType;
+      this.partner_type = institute.institutePartnerType;
+      this.address = institute.instituteAddress;
+      this.price = institute.institutePrice;
+      this.district_id = institute.instituteDistrictId;
+      this.photos_url = institute.institutePhotosUrl;
+      this.events_id = institute.instituteEventsId;
+    }
+
+    toJSON() {
+        return {
+            instituteId: this.institute_id,
+            name: this.name,
+            logoPhoto: this.logo_photo,
+            description: this.description,
+            instituteType: this.institute_type,
+            partnerType: this.partner_type,
+            address: this.address,
+            price: this.price,
+            districtId: this.district_id,
+            photosUrl: this.photos_url,
+            eventsId: this.events_id
+        };
+    }
+}
+
+
+export class GetAllInstitutesViewModel {
+  private institutes: InstituteViewModel[];
+
+  constructor(institutes: Institute[]) {
+    this.institutes = institutes.map((institute) => new InstituteViewModel(institute));
+  }
+
+  toJSON() {
+    return {
+      events: this.institutes.map((institute) => institute.toJSON()),
+      message: "All institutes have been retrieved successfully",
+    };
+  }
+}
