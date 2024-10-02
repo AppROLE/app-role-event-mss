c12cf5d52f8ec09f798e07992f3e5328659c5aed
gabrielmerola <gabrielmerola12@gmail.com>
2024-10-01 23:10:40 -0300

    add getInstituteById to mock

--- src/shared/infra/mocks/institute_repository_mock.ts
+++ src/shared/infra/mocks/institute_repository_mock.ts
@@ -1,6 +1,7 @@
 import { InstituteMock } from "src/shared/domain/mocks/institute_mock";
 import { Institute } from "../../domain/entities/institute";
 import { IInstituteRepository } from "../../domain/irepositories/institute_repository_interface";
+import { NoItemsFound } from "src/shared/helpers/errors/usecase_errors";
 
 export class InstituteRepositoryMock implements IInstituteRepository {
   private institutes: Institute[];
@@ -12,6 +13,14 @@ export class InstituteRepositoryMock implements IInstituteRepository {
 
   createInstitute(institute: Institute): Promise<Institute> {
     this.institutes.push(institute);
+    return Promise.resolve(institute);
+  }
+
+  getInstituteById(instituteId: string): Promise<Institute> {
+    const institute = this.institutes.find((institute) => institute.instituteId === instituteId);
+    if (!institute) {
+      throw new NoItemsFound("institute");
+    }
     return Promise.resolve(institute);
   }
 }
