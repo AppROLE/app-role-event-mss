3de9db4d967a37a0d32f466ebee170bdabc461b0
gabrielmerola <gabrielmerola12@gmail.com>
2024-10-01 22:43:55 -0300

    add get institute by id presenter

---
+++ src/modules/get_institute_by_id/app/get_institute_by_id_presenter.ts
@@ -0,0 +1,28 @@
+import { Environments } from "src/shared/environments";
+import {
+  LambdaHttpRequest,
+  LambdaHttpResponse,
+} from "src/shared/helpers/external_interfaces/http_lambda_requests";
+import { GetInstituteByIdUseCase } from "./get_institute_by_id_usecase";
+import { GetInstituteByIdController } from "./get_institute_by_id_controller";
+
+const repo = Environments.getInstituteRepo();
+const usecase = new GetInstituteByIdUseCase(repo);
+const controller = new GetInstituteByIdController(usecase);
+
+export async function getEventByIdPresenter(event: Record<string, any>) {
+    const httpRequest = new LambdaHttpRequest(event);
+    const response = await controller.handle(httpRequest);
+    const httpResponse = new LambdaHttpResponse(
+      response?.body,
+      response?.statusCode,
+      response?.headers
+    );
+  
+    return httpResponse.toJSON();
+  }
+  
+  export async function lambda_handler(event: any, context: any) {
+    const response = await getEventByIdPresenter(event);
+    return response;
+  }
