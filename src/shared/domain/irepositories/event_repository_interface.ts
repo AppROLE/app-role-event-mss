dd88a4ee92d1e80deeba1a8d51d0e164178b040b
LucaPinheiro <lucapgomes11@gmail.com>
2024-10-02 23:27:31 -0300

    feat: Add getEventsByFilter method to IEventRepository interface

--- src/shared/domain/irepositories/event_repository_interface.ts
+++ src/shared/domain/irepositories/event_repository_interface.ts
@@ -3,6 +3,7 @@ import { Event } from "../entities/event";
 export interface IEventRepository {
   createEvent(event: Event): Promise<Event>;
   getAllEvents(): Promise<Event[]>;
+  getEventsByFilter(filter: any): Promise<Event[]>;
   getEventById(eventId: string): Promise<Event>;
   deleteEventById(eventId: string): Promise<void>;
 }
