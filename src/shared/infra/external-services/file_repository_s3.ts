c23e06f3c8af8783a8e92644f224a184ec7284a3
LucaPinheiro <lucapgomes11@gmail.com>
2024-10-03 11:44:40 -0300

    feat: Implement uploadEventPhoto method in FileRepositoryS3

---
+++ src/shared/infra/external-services/file_repository_s3.ts
@@ -0,0 +1,33 @@
+import { S3 } from "aws-sdk";
+import { IFileRepository } from "src/shared/domain/irepositories/file_repository_interface";
+
+export class FileRepositoryS3 implements IFileRepository {
+  s3BucketName: string;
+
+  constructor(s3BucketName: string) {
+    this.s3BucketName = s3BucketName;
+  }
+
+  async uploadEventPhoto(
+    imageNameKey: string,
+    eventPhoto: Buffer,
+    mimetype: string
+  ): Promise<void> {
+    try {
+      const s3 = new S3();
+      const params: S3.PutObjectRequest = {
+        Bucket: this.s3BucketName,
+        Key: imageNameKey,
+        Body: eventPhoto,
+        ContentType: mimetype,
+      };
+
+      await s3.putObject(params).promise();
+
+    } catch (error: any) {
+      throw new Error(
+        `FileRepositoryS3, Error on uploadEventPhoto: ${error.message}`
+      );
+    }
+  }
+}
