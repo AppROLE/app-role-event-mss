985baaaa65e94a6e229b8b26440801aa7f178f89
LucaPinheiro <lucapgomes11@gmail.com>
2024-10-02 23:27:35 -0300

    feat: Add getEventsByFilter method to EventRepositoryMongo

--- src/shared/infra/database/repositories/event_repository_mongo.ts
+++ src/shared/infra/database/repositories/event_repository_mongo.ts
@@ -57,6 +57,46 @@ export class EventRepositoryMongo implements IEventRepository {
     }
   }
 
+  async getEventsByFilter(filter: any): Promise<Event[]> {
+    try {
+      const db = await connectDB();
+      db.connections[0].on("error", () => {
+        console.error.bind(console, "connection error:");
+        throw new Error("Erro ao conectar ao MongoDB");
+      });
+  
+      const eventMongoClient = db.connections[0].db?.collection<IEvent>("Event");
+  
+      const query: any = {};
+  
+      if (filter.name) query.name = filter.name;
+      if (filter.institute_id) query.institute_id = filter.institute_id;
+      if (filter.price) query.price = filter.price;
+      if (filter.address) query.address = filter.address;
+      if (filter.age_range) query.age_range = filter.age_range;
+      if (filter.event_date) query.event_date = { $gte: new Date(filter.event_date) }; 
+      if (filter.district_id) query.district_id = filter.district_id;
+      if (filter.music_type) query.music_type = { $in: filter.music_type }; 
+      if (filter.features) query.features = { $in: filter.features }; 
+      if (filter.category) query.category = filter.category;
+      if (filter.package_type) query.package_type = { $in: filter.package_type }; 
+      if (filter.ticket_url) query.ticket_url = filter.ticket_url;
+  
+      const eventDocs = (await eventMongoClient?.find(query).toArray()) as IEvent[];
+  
+      if (!eventDocs || eventDocs.length === 0) {
+        throw new NoItemsFound("events");
+      }
+  
+      return eventDocs.map((eventDoc) =>
+        EventMongoDTO.toEntity(EventMongoDTO.fromMongo(eventDoc))
+      );
+    } catch (error) {
+      throw new Error(`Erro ao buscar eventos com filtro no MongoDB: ${error}`);
+    }
+  }
+  
+
   async getEventById(eventId: string): Promise<Event> {
     try {
       const db = await connectDB();
