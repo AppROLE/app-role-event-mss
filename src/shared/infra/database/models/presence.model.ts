3b81aba28985c39754e3dc80282d8aca90ab550b
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-10-03 22:45:50 -0300

    refactor: Add username, nickname, and profile photo fields to Presence model

--- src/shared/infra/database/models/presence.model.ts
+++ src/shared/infra/database/models/presence.model.ts
@@ -5,6 +5,9 @@ export interface IPresence extends Document {
   _id: string;
   user_id: string;
   event_id: string;
+  username: string;
+  nickname: string;
+  profile_photo: string;
   promoter_code: string;
   checked_in_at: Date;
 }
@@ -13,6 +16,9 @@ const PresenceSchema: Schema = new Schema<IPresence>({
   _id: { type: String, default: uuidv4 },
   user_id: { type: String, ref: "User", required: true },
   event_id: { type: String, ref: "Event", required: true },
+  username: { type: String, required: true },
+  nickname: { type: String, required: true },
+  profile_photo: { type: String, required: false },
   promoter_code: { type: String },
   checked_in_at: { type: Date, default: Date.now },
 });
