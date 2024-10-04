4c5ed60f26fd58adbd97411129b9a716cda578d9
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-10-04 00:45:17 -0300

    feat: Add nickname field to UserAPIGatewayDTO constructor and getParsedData method

--- src/shared/infra/dto/user_api_gateway_dto.ts
+++ src/shared/infra/dto/user_api_gateway_dto.ts
@@ -2,23 +2,26 @@ export class UserAPIGatewayDTO {
   userId: string
   username: string
   email: string
+  nickname: string
 
-  constructor(userId: string, username: string, email: string) {
+  constructor(userId: string, username: string, email: string, nickname: string) {
     this.userId = userId
     this.username = username
     this.email = email
+    this.nickname = nickname
   }
 
   static fromAPIGateway(data: Record<string, any>): UserAPIGatewayDTO {
     console.log('data FROM API GATEWAY ', data)
-    return new UserAPIGatewayDTO(data['sub'], data['cognito:username'], data['email'])
+    return new UserAPIGatewayDTO(data['sub'], data['cognito:username'], data['email'], data['nickname'])
   }
 
   getParsedData() {
     return {
       userId: this.userId,
       username: this.username,
-      email: this.email
+      email: this.email,
+      nickname: this.nickname
     }
   }
 }
