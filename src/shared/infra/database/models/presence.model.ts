734aab7edd4c6d22433b6d734b3ed819a5f2eef6
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-10-04 00:45:23 -0300

    refactor: Update Presence model to make profilePhoto and checkedInAt optional

--- src/shared/infra/database/models/presence.model.ts
+++ src/shared/infra/database/models/presence.model.ts
@@ -3,26 +3,24 @@ import { v4 as uuidv4 } from "uuid";
 
 export interface IPresence extends Document {
   _id: string;
-  user_id: string;
   event_id: string;
   username: string;
   nickname: string;
-  profile_photo: string;
-  promoter_code: string;
-  checked_in_at: Date;
+  profile_photo: string | undefined;
+  promoter_code: string | undefined;
+  checked_in_at: Date | undefined;
 }
 
 const PresenceSchema: Schema = new Schema<IPresence>({
   _id: { type: String, default: uuidv4 },
-  user_id: { type: String, ref: "User", required: true },
   event_id: { type: String, ref: "Event", required: true },
-  username: { type: String, required: true },
+  username: { type: String, ref: "User", required: true },
   nickname: { type: String, required: true },
-  profile_photo: { type: String, required: false },
+  profile_photo: { type: String },
   promoter_code: { type: String },
   checked_in_at: { type: Date, default: Date.now },
 });
 
-PresenceSchema.index({ user_id: 1, event_id: 1 });
+PresenceSchema.index({ event_id: 1 });
 
 export default mongoose.model<IPresence>("Presence", PresenceSchema);
