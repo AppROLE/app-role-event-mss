c95c55866c1cb580cff6911ef19aa6fe0a4a934b
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-10-04 00:45:06 -0300

    refactor: Update IPresenceRepository interface to include confirmPresence method

--- src/shared/domain/irepositories/presence_repository_interface.ts
+++ src/shared/domain/irepositories/presence_repository_interface.ts
@@ -2,4 +2,11 @@ import { Presence } from "../entities/presence";
 
 export interface IPresenceRepository {
   getAllPresences(eventId: string): Promise<Presence[]>;
+  confirmPresence(
+    eventId: string,
+    username: string,
+    nickname: string,
+    profilePhoto?: string,
+    promoterCode?: string
+  ): Promise<void>;
 }
