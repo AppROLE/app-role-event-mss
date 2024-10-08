00d153136582db5465bed318c12d03bd1f48ca29
gabrielmerola <gabrielmerola12@gmail.com>
2024-10-07 23:24:15 -0300

    add route to repo interface

--- src/shared/domain/irepositories/institute_repository_interface.ts
+++ src/shared/domain/irepositories/institute_repository_interface.ts
@@ -1,8 +1,10 @@
 import { Institute } from "../entities/institute";
+import { PARTNER_TYPE } from "../enums/partner_type_enum";
 
 export interface IInstituteRepository {
   createInstitute(institute: Institute): Promise<Institute>;
   getAllInstitutes(): Promise<Institute[]>;
+  getAllInstitutesByPartnerType(partnerType: PARTNER_TYPE): Promise<Institute[]>;
   getInstituteById(instituteId: string): Promise<Institute>;
   deleteInstituteById(instituteId: string): Promise<void>;
 }
