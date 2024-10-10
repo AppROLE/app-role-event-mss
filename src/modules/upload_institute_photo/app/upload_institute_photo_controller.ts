7ca5efd4f221cac68a875dae9f967c24ca98e5c7
gabrielmerola <gabrielmerola12@gmail.com>
2024-10-10 09:13:55 -0300

    remove console.log

--- src/modules/upload_institute_photo/app/upload_institute_photo_controller.ts
+++ src/modules/upload_institute_photo/app/upload_institute_photo_controller.ts
@@ -34,7 +34,6 @@ export class UploadInstitutePhotoController {
             }) as string[]
 
             console.log("imagesBuffers: ", imagesBuffers)
-            console.log("base64: ", imagesBuffers[0].toString('base64'))
             console.log("fieldNames: ", fieldNames)
             console.log("mimetypes: ", mimetypes)
 
