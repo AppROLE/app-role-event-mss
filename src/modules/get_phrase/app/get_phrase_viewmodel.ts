79d7f64cdbd9ba5d2794945a2bc878003b49917d
gabrielmerola <gabrielmerola12@gmail.com>
2024-10-10 21:37:07 -0300

    add getPhraseViewmodel

---
+++ src/modules/get_phrase/app/get_phrase_viewmodel.ts
@@ -0,0 +1,12 @@
+export class GetPhraseViewModel {
+    constructor(
+        private readonly phrase: string,
+        private readonly username: string
+    ) {}
+    
+    toJSON() {
+        return {
+            phrase: this.phrase,
+        };
+    }
+}
