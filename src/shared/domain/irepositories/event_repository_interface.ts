4e393e62b0dbd50cae8d456027d4a66c69055911
LucaPinheiro <lucapgomes11@gmail.com>
2024-10-12 20:18:23 -0300

    Refactor event_repository_interface.ts to add methods for updating the event gallery array and counting gallery events

--- src/shared/domain/irepositories/event_repository_interface.ts
+++ src/shared/domain/irepositories/event_repository_interface.ts
@@ -7,4 +7,6 @@ export interface IEventRepository {
   getEventById(eventId: string): Promise<Event>;
   deleteEventById(eventId: string): Promise<void>;
   updateEventPhoto(eventId: string, profilePhoto: string): Promise<string>;
+  updateGalleryArray(evenetId: string, imageKey: string): Promise<void>;
+  countGalleryEvent(eventId: string): Promise<Number>;
 }
