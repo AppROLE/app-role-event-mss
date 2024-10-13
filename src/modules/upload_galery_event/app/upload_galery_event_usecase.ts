1fc402c932298154ea87f14ba38e53b55153baea
LucaPinheiro <lucapgomes11@gmail.com>
2024-10-12 22:25:57 -0300

    Refactor console log statements, update error handling, and add error class in EventRepositoryMongo

--- src/modules/upload_galery_event/app/upload_galery_event_usecase.ts
+++ src/modules/upload_galery_event/app/upload_galery_event_usecase.ts
@@ -3,25 +3,35 @@ import { IFileRepository } from "src/shared/domain/irepositories/file_repository
 import { Environments } from "src/shared/environments";
 
 export class UploadGalleryEventUseCase {
-    constructor(
-        private readonly eventRepo: IEventRepository,
-        private readonly fileRepo: IFileRepository
-    ) {}
+  constructor(
+    private readonly eventRepo: IEventRepository,
+    private readonly fileRepo: IFileRepository
+  ) {}
 
+  async execute(
+    eventId: string,
+    eventPhoto: Buffer,
+    typePhoto: string,
+    mimetype: string
+  ) {
+    const event = await this.eventRepo.getEventById(eventId);
+    const eventName = event.getEventName;
+    const nameFormat = eventName.replace(/\s+/g, "-");
 
-    async execute(eventId: string, eventPhoto: Buffer, typePhoto: string, mimetype: string) {
+    const numberImages = await this.eventRepo.countGalleryEvent(eventId);
 
-        const event = await this.eventRepo.getEventById(eventId);
-        const eventName = event.getEventName;
-        const nameFormat = eventName.replace(/\s+/g, '-');
+    const imageKey = `${nameFormat}-${numberImages.valueOf() + 1}${typePhoto}`;
 
-        const numberImages = await this.eventRepo.countGalleryEvent(eventId);
+    await this.eventRepo.updateGalleryArray(
+      eventId,
+      `${Environments.getEnvs().cloudFrontUrl}/${imageKey}`
+    );
 
-        const imageKey = `${nameFormat}-${numberImages.valueOf() + 1}${typePhoto}`;
-        
-        await this.eventRepo.updateGalleryArray(eventName, `${Environments.getEnvs().cloudFrontUrl}/${imageKey}`);
-        
-        await this.fileRepo.uploadEventGalleryPhoto( eventName, imageKey, eventPhoto, mimetype);
-
-    }
+    await this.fileRepo.uploadEventGalleryPhoto(
+      eventName,
+      imageKey,
+      eventPhoto,
+      mimetype
+    );
+  }
 }
