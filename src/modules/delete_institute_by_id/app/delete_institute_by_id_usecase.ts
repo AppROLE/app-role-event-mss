c74cb6065561c8413cdac690ab44c10737267b77
LucaPinheiro <lucapgomes11@gmail.com>
2024-10-14 20:10:34 -0300

    Refactor DeleteInstituteByIdUseCase to delete associated events before deleting the institute

--- src/modules/delete_institute_by_id/app/delete_institute_by_id_usecase.ts
+++ src/modules/delete_institute_by_id/app/delete_institute_by_id_usecase.ts
@@ -1,15 +1,27 @@
+import { IEventRepository } from "src/shared/domain/irepositories/event_repository_interface";
 import { IInstituteRepository } from "src/shared/domain/irepositories/institute_repository_interface";
 import { NoItemsFound } from "src/shared/helpers/errors/usecase_errors";
 
 export class DeleteInstituteByIdUseCase {
-  constructor(private readonly repo: IInstituteRepository) {}
+  constructor(
+    private readonly instituteRepository: IInstituteRepository,
+    private readonly eventRepository: IEventRepository
+  ) {}
 
   async execute(instituteId: string): Promise<void> {
-    const institute = await this.repo.getInstituteById(instituteId);
+    const institute = await this.instituteRepository.getInstituteById(
+      instituteId
+    );
     if (!institute) {
       throw new NoItemsFound("institute");
     }
 
-    await this.repo.deleteInstituteById(instituteId);
+    if (institute.instituteEventsId) {
+      for (const eventId of institute.instituteEventsId) {
+        await this.eventRepository.deleteEventById(eventId);
+      }
+    }
+    
+    await this.instituteRepository.deleteInstituteById(instituteId);
   }
 }
