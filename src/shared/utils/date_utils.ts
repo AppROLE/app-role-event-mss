e73e1452c5ab04dc0936773272c0073618641783
LucaPinheiro <lucapgomes11@gmail.com>
2024-10-15 19:55:34 -0300

    Refactor getUpcomingWeekdays function to set UTC hours to 0

--- src/shared/utils/date_utils.ts
+++ src/shared/utils/date_utils.ts
@@ -1,20 +1,26 @@
 export function getUpcomingWeekdays() {
-    const today = new Date();
-    const currentDay = today.getDay(); 
-  
-    const daysUntilThursday = (4 - currentDay + 7) % 7;
-    const daysUntilFriday = (5 - currentDay + 7) % 7;
-    const daysUntilSaturday = (6 - currentDay + 7) % 7;
-  
-    const nextThursday = new Date(today);
-    nextThursday.setDate(today.getDate() + daysUntilThursday);
-  
-    const nextFriday = new Date(today);
-    nextFriday.setDate(today.getDate() + daysUntilFriday);
-  
-    const nextSaturday = new Date(today);
-    nextSaturday.setDate(today.getDate() + daysUntilSaturday);
-  
-    return { nextThursday, nextFriday, nextSaturday };
-  }
-  
+  const today = new Date();
+  const currentDay = today.getDay(); 
+
+  const daysUntilThursday = (4 - currentDay + 7) % 7;
+  const daysUntilFriday = (5 - currentDay + 7) % 7;
+  const daysUntilSaturday = (6 - currentDay + 7) % 7;
+
+  const nextThursday = new Date(today);
+  nextThursday.setDate(today.getDate() + daysUntilThursday);
+  nextThursday.setUTCHours(0, 0, 0, 0); 
+
+  const nextFriday = new Date(today);
+  nextFriday.setDate(today.getDate() + daysUntilFriday);
+  nextFriday.setUTCHours(0, 0, 0, 0); 
+
+  const nextSaturday = new Date(today);
+  nextSaturday.setDate(today.getDate() + daysUntilSaturday);
+  nextSaturday.setUTCHours(0, 0, 0, 0); 
+
+  return {
+    nextThursday,
+    nextFriday,
+    nextSaturday
+  };
+}
