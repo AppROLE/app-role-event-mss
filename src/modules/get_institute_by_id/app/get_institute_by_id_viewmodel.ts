f3fab997501710710469ff8a39b5681c741a0e5f
LucaPinheiro <lucapgomes11@gmail.com>
2024-10-14 22:27:45 -0300

    Refactor GetInstituteByIdViewModel to include additional fields and improve code consistency

--- src/modules/get_institute_by_id/app/get_institute_by_id_viewmodel.ts
+++ src/modules/get_institute_by_id/app/get_institute_by_id_viewmodel.ts
@@ -4,13 +4,29 @@ export class GetInstituteByIdViewModel {
     private institute_id: string;
     private name: string;
     private logo_photo: string;
+    private description: string;
+    private institute_type: string; 
+    private partner_type: string; 
+    private phone: string;
+    private address: string;
+    private price: number;
+    private district_id: string;
+    private photos_url: string[];
     private events_id: string[];
 
     constructor(institute: Institute) {
-        this.institute_id = institute.instituteId ?? ''
-        this.name = institute.instituteName
-        this.logo_photo = institute.instituteLogoPhoto ?? ''
-        this.events_id = institute.instituteEventsId ?? []
+        this.institute_id = institute.instituteId ?? '';
+        this.name = institute.instituteName;
+        this.logo_photo = institute.instituteLogoPhoto ?? '';
+        this.description = institute.instituteDescription; 
+        this.institute_type = institute.instituteInstituteType; 
+        this.partner_type = institute.institutePartnerType; 
+        this.phone = institute.institutePhone ?? ''; 
+        this.address = institute.instituteAddress ?? ''; 
+        this.price = institute.institutePrice ?? 0; 
+        this.district_id = institute.instituteDistrictId ?? ''; 
+        this.photos_url = institute.institutePhotosUrl ?? []; 
+        this.events_id = institute.instituteEventsId ?? [];
     }
 
     toJSON() {
@@ -18,6 +34,14 @@ export class GetInstituteByIdViewModel {
             institute_id: this.institute_id,
             name: this.name,
             logo_photo: this.logo_photo,
+            description: this.description, 
+            institute_type: this.institute_type, 
+            partner_type: this.partner_type, 
+            phone: this.phone, 
+            address: this.address, 
+            price: this.price, 
+            district_id: this.district_id, 
+            photos_url: this.photos_url, 
             events_id: this.events_id
         }
     }
