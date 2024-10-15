71417b78417b4b83311f38b2d2117fed784de1ba
LucaPinheiro <lucapgomes11@gmail.com>
2024-10-15 16:02:08 -0300

    Refactor get_top_events_controller to handle errors and return appropriate HTTP codes

---
+++ src/modules/get_top_events/app/get_top_events_controller.ts
@@ -0,0 +1,32 @@
+import {
+  OK,
+  InternalServerError,
+  NotFound,
+} from "src/shared/helpers/external_interfaces/http_codes";
+import { GetTopEventsUseCase } from "./get_top_events_usecase";
+import { IRequest } from "src/shared/helpers/external_interfaces/external_interface";
+import { NoItemsFound } from "src/shared/helpers/errors/usecase_errors";
+
+export class GetTopEventsController {
+  constructor(private readonly usecase: GetTopEventsUseCase) {}
+
+  async handle(req: IRequest): Promise<any> {
+    try {
+      const topEventsByDate = await this.usecase.execute();
+
+      return new OK({
+        message: "Top events retrieved successfully",
+        data: topEventsByDate,
+      });
+    } catch (error: any) {
+      if (error instanceof NoItemsFound) {
+        return new NotFound(error.message);
+      }
+      if (error instanceof Error) {
+        return new InternalServerError(
+          `CreateEventController, Error on handle: ${error.message}`
+        );
+      }
+    }
+  }
+}
