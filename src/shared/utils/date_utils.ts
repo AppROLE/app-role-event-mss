8fc0b33ecc1387bb0aab5af7b61957d784127143
LucaPinheiro <lucapgomes11@gmail.com>
2024-10-15 21:33:27 -0300

    Refactor getUpcomingWeekdays function in date_utils.ts

--- src/shared/utils/date_utils.ts
+++ src/shared/utils/date_utils.ts
@@ -28,31 +28,3 @@ export function getUpcomingWeekdays() {
   return { nextThursday, nextFriday, nextSaturday };
 }
 
-
-
-// export function getUpcomingWeekdays() {
-//   const today = new Date();
-//   const currentDay = today.getDay(); 
-
-//   const daysUntilThursday = (4 - currentDay + 7) % 7;
-//   const daysUntilFriday = (5 - currentDay + 7) % 7;
-//   const daysUntilSaturday = (6 - currentDay + 7) % 7;
-
-//   const nextThursday = new Date(today);
-//   nextThursday.setDate(today.getDate() + daysUntilThursday);
-//   nextThursday.setUTCHours(0, 0, 0, 0); 
-
-//   const nextFriday = new Date(today);
-//   nextFriday.setDate(today.getDate() + daysUntilFriday);
-//   nextFriday.setUTCHours(0, 0, 0, 0); 
-
-//   const nextSaturday = new Date(today);
-//   nextSaturday.setDate(today.getDate() + daysUntilSaturday);
-//   nextSaturday.setUTCHours(0, 0, 0, 0); 
-
-//   return {
-//     nextThursday,
-//     nextFriday,
-//     nextSaturday
-//   };
-// }
