9309d8c7cadb37c7e2022e0830d8ebe8b8d9787b
LucaPinheiro <lucapgomes11@gmail.com>
2024-10-16 20:18:34 -0300

    Refactor FileRepositoryS3 to add deleteEventPhotoByEventName method

--- src/shared/infra/external-services/file_repository_s3.ts
+++ src/shared/infra/external-services/file_repository_s3.ts
@@ -54,4 +54,56 @@ export class FileRepositoryS3 implements IFileRepository {
       );
     }
   }
+
+  async deleteEventPhotoByEventName(filename: string): Promise<void> {
+    try {
+      const s3 = new S3();
+      console.log("s3BucketName: ", this.s3BucketName);
+
+      const eventName = filename.substring(
+        filename.lastIndexOf("-") + 1,
+        filename.lastIndexOf(".")
+      ); 
+
+      if (!eventName) {
+        throw new Error("Nome do evento não encontrado no nome do arquivo.");
+      }
+
+      const listParams: S3.ListObjectsV2Request = {
+        Bucket: this.s3BucketName,
+      };
+
+      const listedObjects = await s3.listObjectsV2(listParams).promise();
+
+      if (!listedObjects.Contents || listedObjects.Contents.length === 0) {
+        throw new Error("Nenhum arquivo encontrado no bucket.");
+      }
+
+      const matchingFiles = listedObjects.Contents.filter((file) =>
+        file.Key?.includes(eventName)
+      );
+
+      if (matchingFiles.length === 0) {
+        throw new Error(
+          `Nenhum arquivo encontrado com o nome do evento: ${eventName}`
+        );
+      }
+
+      const deletePromises = matchingFiles.map(async (file) => {
+        const deleteParams: S3.DeleteObjectRequest = {
+          Bucket: this.s3BucketName,
+          Key: file.Key!,
+        };
+
+        await s3.deleteObject(deleteParams).promise();
+        console.log(`Foto deletada com sucesso: ${file.Key}`);
+      });
+
+      await Promise.all(deletePromises);
+    } catch (error: any) {
+      throw new Error(
+        `FileRepositoryS3, Error on deleteEventPhotoByEventName: ${error.message}`
+      );
+    }
+  }
 }
