3fd0fc105e1c79886ff0fcf44d52ed11264590bf
LucaPinheiro <lucapgomes11@gmail.com>
2024-10-16 20:18:21 -0300

    Refactor delete_event_photo_viewmodel to add a toJSON method

---
+++ src/modules/delete_event_photo/app/delete_event_photo_viewmodel.ts
@@ -0,0 +1,14 @@
+export class DeleteEventPhotoByIdViewModel {
+    message: string;
+  
+    constructor(message: string) {
+      this.message = message;
+    }
+  
+    toJSON() {
+      return {
+        message: this.message,
+      };
+    }
+  }
+  
