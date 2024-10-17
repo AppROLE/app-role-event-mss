58ce5cab3d296dd57ae4e685b43682511d97bb29
gabrielmerola <gabrielmerola12@gmail.com>
2024-10-17 09:58:31 -0300

    Refactor CreateReviewUseCase to include name and photoUrl in review properties

--- src/modules/create_review/app/create_review_usecase.ts
+++ src/modules/create_review/app/create_review_usecase.ts
@@ -9,7 +9,9 @@ export class CreateReviewUseCase {
         review: string,
         reviewedAt: number,
         eventId: string,
-        username: string
+        username: string,
+        name: string,
+        photoUrl: string
     ) {
         if (star < 0 || star > 5) {
             throw new EntityError("star");
@@ -23,9 +25,15 @@ export class CreateReviewUseCase {
         if (eventId.length < 1 || eventId.trim() === "") {
             throw new EntityError("eventId");
         }
+        if (username.length < 1 || username.trim() === "") {
+            throw new EntityError("username");
+        }
+        if (photoUrl.length < 1 || photoUrl.trim() === "") {
+            throw new EntityError("photoUrl");
+        }
 
         const reviwedDate = new Date(reviewedAt);
 
-        await this.repo.createReview(star, review, reviwedDate, eventId, username);
+        await this.repo.createReview(star, review, reviwedDate, eventId, name, photoUrl, username);
     }
 }
