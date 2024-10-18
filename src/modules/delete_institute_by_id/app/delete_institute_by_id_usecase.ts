bd6e84c2b1c1fb019a1355b48a5ac38a616c96bc
gabrielmerola <gabrielmerola12@gmail.com>
2024-10-18 16:02:16 -0300

    Refactor delete_institute_by_id_usecase.ts to update dependencies and improve code readability

--- src/modules/delete_institute_by_id/app/delete_institute_by_id_usecase.ts
+++ src/modules/delete_institute_by_id/app/delete_institute_by_id_usecase.ts
@@ -1,11 +1,13 @@
 import { IEventRepository } from "src/shared/domain/irepositories/event_repository_interface";
+import { IFileRepository } from "src/shared/domain/irepositories/file_repository_interface";
 import { IInstituteRepository } from "src/shared/domain/irepositories/institute_repository_interface";
 import { NoItemsFound } from "src/shared/helpers/errors/usecase_errors";
 
 export class DeleteInstituteByIdUseCase {
   constructor(
     private readonly instituteRepository: IInstituteRepository,
-    private readonly eventRepository: IEventRepository
+    private readonly eventRepository: IEventRepository,
+    private readonly fileRepository: IFileRepository
   ) {}
 
   async execute(instituteId: string): Promise<void> {
@@ -24,6 +26,9 @@ export class DeleteInstituteByIdUseCase {
         console.log("AQUI CACETEEEEEEEEEEE PORRA ", eventId);
         await this.eventRepository.deleteEventById(eventId);
       }
+    }
+    if(institute.instituteLogoPhoto !== ''){
+      await this.fileRepository.deleteInstitutePhoto(institute.instituteName);
     }
 
     await this.instituteRepository.deleteInstituteById(instituteId);
