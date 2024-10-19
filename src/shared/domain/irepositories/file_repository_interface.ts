e184d80231cb337079c31b23586e04c719f35e42
LucaPinheiro <lucapgomes11@gmail.com>
2024-10-19 17:37:07 -0300

    Refactor deleteEventPhotoByEventName to use deleteEventPhotoByEventId in IFileRepository

--- src/shared/domain/irepositories/file_repository_interface.ts
+++ src/shared/domain/irepositories/file_repository_interface.ts
@@ -1,5 +1,5 @@
 export interface IFileRepository {
   uploadEventPhoto(imageNameKey: string, eventPhoto: Buffer, mimetype: string): Promise<void>;
   uploadEventGalleryPhoto(eventName: string, imageNameKey: string, eventPhoto: Buffer, mimetype: string): Promise<void>;
-  deleteEventPhotoByEventName(filename: string): Promise<void>;
+  deleteEventPhotoByEventId(eventId: string): Promise<void>;
 }
