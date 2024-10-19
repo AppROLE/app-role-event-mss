014303536b9b4dbbeeff352c6ddc988ec705fa98
LucaPinheiro <lucapgomes11@gmail.com>
2024-10-19 17:37:03 -0300

    Refactor DeleteEventPhotoUseCase to use eventId instead of filename and add eventRepository dependency

--- src/modules/delete_event_photo/app/delete_event_photo_usecase.ts
+++ src/modules/delete_event_photo/app/delete_event_photo_usecase.ts
@@ -1,9 +1,18 @@
+import { IEventRepository } from "src/shared/domain/irepositories/event_repository_interface";
 import { IFileRepository } from "src/shared/domain/irepositories/file_repository_interface";
+import { NoItemsFound } from "src/shared/helpers/errors/usecase_errors";
 
 export class DeleteEventPhotoUseCase {
-  constructor(private readonly fileRepository: IFileRepository) {}
+  constructor(
+    private readonly fileRepository: IFileRepository,
+    private readonly eventRepository: IEventRepository
+  ) {}
 
-  async execute(filename: string): Promise<void> {
-    await this.fileRepository.deleteEventPhotoByEventName(filename);
+  async execute(eventId: string): Promise<void> {
+    const event = await this.eventRepository.getEventById(eventId);
+    if (!event) {
+      throw new NoItemsFound("event");
+    }
+    await this.fileRepository.deleteEventPhotoByEventId(eventId);
   }
 }
