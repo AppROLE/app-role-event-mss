69a00064dfd6640fccb82c2f8c49b49c108f2eda
LucaPinheiro <lucapgomes11@gmail.com>
2024-10-22 20:30:34 -0300

    refactor: Update uploadEventGalleryPhoto signature in file_repository_interface.ts

--- src/shared/domain/irepositories/file_repository_interface.ts
+++ src/shared/domain/irepositories/file_repository_interface.ts
@@ -1,6 +1,6 @@
 export interface IFileRepository {
   uploadEventPhoto(imageNameKey: string, eventPhoto: Buffer, mimetype: string): Promise<void>;
-  uploadEventGalleryPhoto(eventName: string, imageNameKey: string, eventPhoto: Buffer, mimetype: string): Promise<void>;
+  uploadEventGalleryPhoto(eventId: string, eventName: string, imageNameKey: string, eventPhoto: Buffer, mimetype: string): Promise<void>;
   deleteEventPhotoByEventId(eventId: string): Promise<void>;
   deleteInstitutePhoto(name: string): Promise<void>;
 }
