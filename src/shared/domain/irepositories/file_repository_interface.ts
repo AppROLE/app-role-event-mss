03e94a327a98c41d168d00e583651e4c17bdc87f
LucaPinheiro <lucapgomes11@gmail.com>
2024-10-22 20:47:11 -0300

    refactor: Add deleteGallery method to file repository interface

--- src/shared/domain/irepositories/file_repository_interface.ts
+++ src/shared/domain/irepositories/file_repository_interface.ts
@@ -2,5 +2,6 @@ export interface IFileRepository {
   uploadEventPhoto(imageNameKey: string, eventPhoto: Buffer, mimetype: string): Promise<void>;
   uploadEventGalleryPhoto(eventId: string, eventName: string, imageNameKey: string, eventPhoto: Buffer, mimetype: string): Promise<void>;
   deleteEventPhotoByEventId(eventId: string): Promise<void>;
+  deleteGallery(eventId: string): Promise<void>;
   deleteInstitutePhoto(name: string): Promise<void>;
 }
