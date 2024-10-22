c0de830421e345606be8761add658e6639af8878
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-10-21 23:42:44 -0300

    feat: Update unconfirmEventPresenter to handle unconfirming event attendance

--- src/modules/unconfirm_event/app/unconfirm_event_presenter.ts
+++ src/modules/unconfirm_event/app/unconfirm_event_presenter.ts
@@ -1,1 +1,34 @@
-// Conteúdo do arquivo...
+import { Environments } from "src/shared/environments";
+import {
+  LambdaHttpRequest,
+  LambdaHttpResponse,
+} from "src/shared/helpers/external_interfaces/http_lambda_requests";
+import { UnConfirmEventController } from "./unconfirm_event_controller";
+import { UnConfirmEventUseCase } from "./unconfirm_event_usecase";
+import { getRequesterUser } from "src/shared/utils/get_requester_user";
+
+const eventRepo = Environments.getEventRepo();
+const presenceRepo = Environments.getPresenceRepo();
+const usecase = new UnConfirmEventUseCase(eventRepo, presenceRepo);
+const controller = new UnConfirmEventController(usecase);
+
+export async function unconfirmEventPresenter(
+  event: Record<string, any>
+) {
+  const requesterUser = getRequesterUser(event);
+  const httpRequest = new LambdaHttpRequest(event);
+  const response = await controller.handle(httpRequest, requesterUser);
+  const httpResponse = new LambdaHttpResponse(
+    response?.body,
+    response?.statusCode,
+    response?.headers
+  );
+
+  return httpResponse.toJSON();
+}
+
+
+export async function lambda_handler(event: any, context: any) {
+  const response = await unconfirmEventPresenter(event);
+  return response;
+}
